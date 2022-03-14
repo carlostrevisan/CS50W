de7e0055350cd5012b13a4e5408d778692722278
Carlos <carlosedutrevisan@gmail.com>
2022-03-14 11:12:54 -0300

    Add lecture 3

---
+++ lecture3/hello/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "hello/index.html")
+
+
+def carlos(request):
+    return HttpResponse("Hello, Carlos!")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+
+def greet(request, name):
+    return render(
+        request,
+        "hello/greet.html",
+        {
+            "name": name.capitalize(),
+        },
+    )
