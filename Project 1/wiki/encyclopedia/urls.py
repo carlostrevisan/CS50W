9a0fe36de71a07c91e08abd6b3d81a2bf51859fd
carlosedutrevisan <71397052+carlosedutrevisan@users.noreply.github.com>
2022-03-24 20:28:44 -0300

    Finish Project 1

--- Project 1/wiki/encyclopedia/urls.py
+++ Project 1/wiki/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.wiki, name="wiki"),
     path("create", views.create, name="create"),
     path("wiki/", views.random, name="random"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
 ]
 
--- Project 1/wiki/encyclopedia/views.py
+++ Project 1/wiki/encyclopedia/views.py
@@ -5,15 +5,29 @@ from django import forms
 from . import util
 from markdown2 import Markdown
 import random
+from django.db import models
 
 
 class SearchForm(forms.Form):
     search = forms.CharField(label="")
 
 
+class CreateForm(forms.Form):
+    title = forms.CharField(
+        label="", widget=forms.TextInput(attrs={"id": "title", "size": 80})
+    )
+    content = forms.CharField(
+        label="",
+        widget=forms.Textarea(
+            attrs={
+                "id": "content",
+            }
+        ),
+    )
+
+
 def index(request):
     if request.method == "POST":
-        print("procurou")
         form = SearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data["search"]
@@ -34,14 +48,12 @@ def index(request):
                     if query.lower() in entry.lower():
                         entries_found.append(entry)
                 print(entries_found)
+                entries_found.sort()
+                print(entries_found)
                 return render(
                     request,
                     "encyclopedia/notfound.html",
-                    {
-                        "title": query,
-                        "form": SearchForm(),
-                        "entries": entries_found
-                    },
+                    {"title": query, "form": SearchForm(), "entries": entries_found},
                 )
 
     return render(
@@ -52,17 +64,33 @@ def index(request):
 
 
 def create(request):
-    return render(request, "encyclopedia/create.html", {"form": SearchForm()})
+    if request.method == "POST":
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return HttpResponseRedirect(f"/wiki/{title}")
+    return render(
+        request,
+        "encyclopedia/create.html",
+        {"form": SearchForm(), "createForm": CreateForm()},
+    )
 
 
 def wiki(request, title):
+    stuff = Markdown().convert(util.get_entry(title))
     if util.get_entry(title) is None:
         return render(request, "encyclopedia/notfound.html", {"form": SearchForm()})
     else:
         return render(
             request,
             "encyclopedia/wiki.html",
-            {"title": title, "stuff": Markdown().convert(util.get_entry(title)), "form": SearchForm()},
+            {
+                "title": title,
+                "stuff": stuff,
+                "form": SearchForm(),
+            },
         )
 
 
@@ -78,6 +106,7 @@ def search(request, search):
             },
         )
 
+
 def random(request):
     list = util.list_entries()
     select = choice(list)
@@ -85,3 +114,22 @@ def random(request):
         return render(request, "encyclopedia/notfound.html", {"form": SearchForm()})
     else:
         return HttpResponseRedirect(f"/wiki/{select}")
+
+
+def edit(request, title):
+    if request.method == "POST":
+        form = CreateForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            util.save_entry(title, content)
+            return HttpResponseRedirect(f"/wiki/{title}")
+    
+    stuff = util.get_entry(title)
+    createForm = CreateForm(initial={'title': title, 'content': stuff})
+    
+    return render(
+        request,
+        "encyclopedia/create.html",
+        {"form": SearchForm(), "createForm": createForm},
+    )
