ac4a0bb4ccd5a45a01605c579408234528fd402e
carlosedutrevisan <71397052+carlosedutrevisan@users.noreply.github.com>
2022-04-12 21:05:26 -0300

    Project 2

--- Project 2/commerce/auctions/models.py
+++ Project 2/commerce/auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bids(models.Model):
+    user = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    listingid = models.IntegerField(primary_key=True)
+    bid = models.IntegerField()
+
+class Listing(models.Model):
+    seller = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    starting_bid = models.IntegerField()
+    category = models.CharField(max_length=64)
+    image_link = models.CharField(
+        max_length=200, default=None, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
--- Project 2/commerce/auctions/views.py
+++ Project 2/commerce/auctions/views.py
@@ -3,12 +3,18 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import User
+from requests import RequestException
+from django.contrib.auth.decorators import login_required
+from .models import Listing, User
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    products = Listing.objects.all()
+    return render(
+            request,
+            "auctions/index.html",
+            {"products": products},
+        )
 
 
 def login_view(request):
@@ -24,9 +30,11 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
+            return render(
+                request,
+                "auctions/login.html",
+                {"message": "Invalid username and/or password."},
+            )
     else:
         return render(request, "auctions/login.html")
 
@@ -45,19 +53,47 @@ def register(request):
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+            return render(
+                request, "auctions/register.html", {"message": "Passwords must match."}
+            )
 
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
             user.save()
         except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
+            return render(
+                request,
+                "auctions/register.html",
+                {"message": "Username already taken."},
+            )
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+@login_required(login_url="/login")
+def createlisting(request):
+    if request.method == "POST":
+        item = Listing()
+        item.seller = request.user.username
+        item.title = request.POST.get("title")
+        item.description = request.POST.get("description")
+        item.category = request.POST.get("category")
+        item.starting_bid = request.POST.get("starting_bid")
+        item.image_link = request.POST.get("image_link")
+        item.save()
+        products = Listing.objects.all()
+        if len(products) == 0:
+            empty = True
+        else:
+            empty = False
+        print(item)
+        return render(
+            request,
+            "auctions/index.html",
+            {"products": products, "empty": empty},
+        )
+    else:
+        return render(request, "auctions/createlisting.html")
